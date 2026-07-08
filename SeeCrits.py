f231d01164e35497c68d0b48a5ea6653134f4eeb
DanielTheSilly <faceshed@gmail.com>
2026-07-09 01:29:38 +0200

    Add files via upload

--- SeeCrits.py
+++ SeeCrits.py
@@ -14,7 +14,7 @@ FONT_SIZE = int(TILE_SIZE*0.8)
 class ZooWindow(arcade.Window):
 
     rand = random.Random()
-
+        
     def load_crits(self):
         found_crits = []
         
@@ -31,6 +31,7 @@ class ZooWindow(arcade.Window):
     def __init__(self):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "See Crits")
         self.critters = [[None for _ in range(int(SCREEN_HEIGHT / TILE_SIZE))] for _ in range(int(SCREEN_WIDTH / TILE_SIZE))]
+        self.facing = [[1 for _ in range(int(SCREEN_HEIGHT / TILE_SIZE))] for _ in range(int(SCREEN_WIDTH / TILE_SIZE))]
         self.is_running = False
         self.critList = self.load_crits()
 
@@ -73,8 +74,124 @@ class ZooWindow(arcade.Window):
             self.tick_simulation()
 
     def tick_simulation(self):
-        pass
-        # TODO Update grid logic here
+        # Generate all possible coordinate pairs (x, y)
+        coords = [(x, y) for x in range(len(self.critters)) for y in range(len(self.critters[x]))]
+
+        # Shuffle the list of coordinates in-place
+        random.shuffle(coords)
+        print("one tick")
+
+        # Iterate in random order
+        for x, y in coords:
+            critter = self.critters[x][y]
+            face = self.facing[x][y]
+            # Get local view for cirt
+            if critter != None:
+                if x == 0:
+                    west = enum.WALL
+                else:
+                    ocritter = self.critters[x-1][y]
+                    if ocritter == None:
+                        west = enum.EMPTY
+                    else:
+                        if type(ocritter) == type(critter):
+                            west = enum.FRIEND
+                        west = enum.ENEMY
+                if x == len(self.critters)-1:
+                    east = enum.WALL
+                else:
+                    ocritter = self.critters[x+1][y]
+                    if ocritter == None:
+                        east = enum.EMPTY
+                    else:
+                        if type(ocritter) == type(critter):
+                            east = enum.FRIEND
+                        east = enum.ENEMY
+                if y == 0:
+                    north = enum.WALL
+                else:
+                    ocritter = self.critters[x][y-1]
+                    if ocritter == None:
+                        north = enum.EMPTY
+                    else:
+                        if type(ocritter) == type(critter):
+                            north = enum.FRIEND
+                        north = enum.ENEMY
+                if y == len(self.critters[x])-1:
+                    south = enum.WALL
+                else:
+                    ocritter = self.critters[x][y+1]
+                    if ocritter == None:
+                        south = enum.EMPTY
+                    else:
+                        if type(ocritter) == type(critter):
+                            south = enum.FRIEND
+                        south = enum.ENEMY
+                # Get move
+                # TODO name the numbes in facing with enums
+                if face == 0:
+                    move = critter.move(north, west, east, south)
+                elif face == 1:
+                    move = critter.move(east, north, south, west)
+                elif face == 2:
+                    move = critter.move(south, east, west, north)
+                else: # elif face == 3:
+                    move = critter.move(west, south, north, east)
+                # Update grid
+                if move == enum.WAIT:
+                    pass
+                elif move == enum.LEFT:
+                    face = face -1
+                    if face == -1:
+                        face = 3
+                    pass
+                elif move == enum.RIGHT:
+                    face = face +1
+                    if face == 4:
+                        face = 0
+                    pass
+                elif move == enum.FORWARD:
+                    if face == 0:
+                        if north == enum.WALL or north == enum.FRIEND:
+                            pass
+                        elif north == enum.ENEMY:
+                            self.critters[x][y-1] = critter()
+                            pass
+                        elif north == enum.EMPTY:
+                            self.critters[x][y] = None
+                            self.critters[x][y-1] = critter
+                            pass
+                    elif face == 1:
+                        if east == enum.WALL or east == enum.FRIEND:
+                            pass
+                        elif east == enum.ENEMY:
+                            self.critters[x][y-1] = critter()
+                            pass
+                        elif east == enum.EMPTY:
+                            self.critters[x][y] = None
+                            self.critters[x][y-1] = critter
+                            pass
+                    elif face == 2:
+                        if south == enum.WALL or south == enum.FRIEND:
+                            pass
+                        elif south == enum.ENEMY:
+                            self.critters[x][y-1] = critter()
+                            pass
+                        elif south == enum.EMPTY:
+                            self.critters[x][y] = None
+                            self.critters[x][y-1] = critter
+                            pass
+                    else:
+                        if west == enum.WALL or west == enum.FRIEND:
+                            pass
+                        elif west == enum.ENEMY:
+                            self.critters[x][y-1] = critter()
+                            pass
+                        elif west == enum.EMPTY:
+                            self.critters[x][y] = None
+                            self.critters[x][y-1] = critter
+                            pass
+        print("...done")
         # print("Ticking one step...")
     
     def populate(self):
@@ -92,6 +209,7 @@ class ZooWindow(arcade.Window):
                 x = self.rand.randint(0, len(self.critters)-1)
                 y = self.rand.randint(0, len(self.critters[x])-1)
             self.critters[x][y] = self.critList[j]()
+            self.facing[x][y] = self.rand.randint(0,3)
             if j >= len(self.critList):
                 j = 0
 
